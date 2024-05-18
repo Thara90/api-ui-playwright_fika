8efe8d87f13938fa04feafef98f5c9469ae1ec45
thara <tpshadinijk@gmail.com>
2024-05-18 23:23:02 +0530

    env file changes

--- tests/api-ui.spec.ts
+++ tests/api-ui.spec.ts
@@ -3,7 +3,7 @@ import * as loginPayload from '../req-jsons/loginPayload.json';
 import * as cartPayloadData from '../req-jsons/cartPayload.json';
 import path from 'path';
 import fs from 'fs/promises';
-let token, cartId;
+let token, cartId, product_price;
 
 let cartPayload = { ...cartPayloadData };
 
@@ -13,33 +13,38 @@ test.describe('Example Test Suite', () => {
 
         //Login API
         const apiContext = await request.newContext();
-        const loginResponse = await apiContext.post("https://api.practicesoftwaretesting.com/users/login",
+        const loginResponse = await apiContext.post(`${process.env.BASE_URL}/users/login`,
             {
-                data: loginPayload
+                data: {
+                    email: `${process.env.USERNAME}`,
+                    password: `${process.env.PASSWORD}`
+                }
             }
         )
         expect(loginResponse.ok()).toBeTruthy();
         const loginResponseJson = await loginResponse.json();
         token = loginResponseJson.access_token;
-        console.log(loginResponseJson);
+        console.log("##########################\n" + token + "\n##########################");
 
         //Get product id
-        const productDetailsResponse = await apiContext.get("https://api.practicesoftwaretesting.com/products?between=price,1,100&page=1",
+        const productDetailsResponse = await apiContext.get(`${process.env.BASE_URL}/products?between=price,1,100&page=1`,
             {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
-                },
-                data: loginPayload
+                }
             }
         )
         expect(loginResponse.ok()).toBeTruthy();
         const productDetailsResponseJson = await productDetailsResponse.json();
         const product_name = productDetailsResponseJson.data[0].name;
         const product_id = productDetailsResponseJson.data[0].id;
-        console.log('product_name : ' + product_name);
+        product_price = productDetailsResponseJson.data[0].price;
+        console.log("\n" + "##########################\n" + 'product_name : ' + product_name);
         console.log('product_id : ' + product_id);
+        console.log('product_id : ' + product_price + "\n##########################");
 
+        //writting product details into carePayload.json
         cartPayload.product_id = product_id;
         const filePath = path.join(__dirname, "../req-jsons", "cartPayload.json");
         const existingData = await fs.readFile(filePath, 'utf-8');
@@ -48,7 +53,7 @@ test.describe('Example Test Suite', () => {
         await fs.writeFile(filePath, JSON.stringify(existingPayload, null, 2));
 
         //Create cart
-        const cartResponse = await apiContext.post("https://api.practicesoftwaretesting.com/carts",
+        const creatCartResponse = await apiContext.post(`${process.env.BASE_URL}/carts`,
             {
                 headers: {
                     'Authorization': `Bearer ${token}`,
@@ -56,31 +61,30 @@ test.describe('Example Test Suite', () => {
                 }
             }
         )
-        const cartResponseJson = await cartResponse.json();
-        cartId = cartResponseJson.id;
-        console.log(cartResponseJson);
-
-        console.log('cart payload : ' + cartPayload);
+        const creatCartResponseJson = await creatCartResponse.json();
+        cartId = creatCartResponseJson.id;
+        console.log("\n" + "##########################\n" + 'cart ID : ' + cartId + "\n##########################");
 
         //Add to Cart
-        const orderResponse = await apiContext.post(`https://api.practicesoftwaretesting.com/carts/${cartId}`,
+        const addToCartResponse = await apiContext.post(`${process.env.BASE_URL}/carts/${cartId}`,
             {
-                data: cartPayload,
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json'
-                }
+                },
+                data: cartPayload
             }
         )
-        const orderResponseJson = await orderResponse.json();
-        console.log(orderResponseJson);
+        const addToCartResponseJson = await addToCartResponse.json();
+        console.log("\n" + "##########################\n");
+        console.log(addToCartResponseJson);
     });
 
     test('Verify item price in cart', async ({ page }) => {
         page.addInitScript(value => {
             window.localStorage.setItem('auth-token', value);
         }, token);
-        await page.goto("https://practicesoftwaretesting.com/#/checkout");
+        await page.goto(`${process.env.WEB_URL}/#/checkout`);
         page.addInitScript(value => {
             window.sessionStorage.setItem('cart_id', value);
         }, cartId);
@@ -88,7 +92,7 @@ test.describe('Example Test Suite', () => {
             window.sessionStorage.setItem('cart_quantity', value);
         }, '1');
         await page.reload();
-        await expect.soft(page.locator('//tbody/tr/td[4]/span[1]')).toContainText('$14.15');
+        await expect.soft(page.locator('//tbody/tr/td[4]/span[1]')).toContainText(`${product_price}`);
         //await page.pause();
     });
 });
