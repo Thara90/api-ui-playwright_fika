51bf3a5de3242dcfdae9364633e2fa04ceb2dcf6
thara <tpshadinijk@gmail.com>
2024-05-28 18:54:31 +0530

    code enhancements

--- tests/messages.spec.ts
+++ tests/messages.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test.describe('Customer and Admin Operations', () => {
 
     test('Verify message flow between admin and customer', async ({ browser }) => {
-        let msgSubject = 'Return';
+        let msgSubject = 'Warranty';
         // Create a new browser context for the customer with saved authentication state
         const customerContext = await browser.newContext({ storageState: 'customerAuth.json' });
         const customerPage = await customerContext.newPage();
@@ -28,14 +28,13 @@ test.describe('Customer and Admin Operations', () => {
         let lowerCasemsgSubject: string = msgSubject.toLowerCase();
         await adminPage.locator(`(//td[text()='${lowerCasemsgSubject}']/ancestor::tr)[1]//a`).click();
         await expect.soft(adminPage.locator(`//h2[text()='Replies']`)).toBeVisible();
-        // await expect(adminPage.locator("//p[text()='Hi This is a test message to get customer service.']")).toHaveText("Hi This is a test message to get customer service.");
         await adminPage.locator('[data-test="message"]').fill('Hi, This is a test reply for customer from admin.');
         await adminPage.locator('[data-test="reply-submit"]').click();
 
         //Customer operations to check the reply
-        //await customerPage.goto('https://practicesoftwaretesting.com/#/');
-        // await customerPage.locator('[data-test="nav-messages"]').click();
-        // await customerPage.getByRole('cell', { name: 'Details' }).first().click();
-        // await expect(customerPage.getByText('Hi, This is a test reply for')).toHaveText("Hi, This is a test reply for the message inquiry.");
+        await customerPage.goto('https://practicesoftwaretesting.com/#/account/messages');
+        await customerPage.locator(`(//td[text()='${lowerCasemsgSubject}']/ancestor::tr)[1]//a`).click();
+        await expect.soft(customerPage.locator(`//h2[text()='Replies']`)).toBeVisible();
+        await expect.soft(customerPage.locator(`(//p[text()='Hi, This is a test reply for customer from admin.'])[1]`)).toBeVisible();
     });
 });
